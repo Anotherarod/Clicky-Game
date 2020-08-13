89252e95d33a44fc7e37feeae12b40b53beb7511
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-08-13 15:32:07 -0700

    Updates

---
+++ precache-manifest.466f8e7329a2a352fe36bfd8ee91d692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89f3a431df05c26913402e786a4058d",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "d4644d603e802684ad75",
+    "url": "/Clicky-Game/static/css/main.229292bf.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/Clicky-Game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4644d603e802684ad75",
+    "url": "/Clicky-Game/static/js/main.f06137a3.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  }
+]);
